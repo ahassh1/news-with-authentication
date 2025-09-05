ef62db6a779ba25c342fe064370c5d7ae9b828d0
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-09-05 11:06:51 +0600

    register done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,6 +5,13 @@ const Register = () => {
   const handleRegister = (e) => {
     e.preventDefault();
     console.log(e.target);
+    const form = e.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log({ name, photo, email, password });
   };
   return (
     <div>
@@ -17,16 +24,40 @@ const Register = () => {
             <fieldset className="fieldset">
               {/* name  */}
               <label className="label">Name</label>
-              <input type="text" className="input" placeholder="name" />
+              <input
+                type="text"
+                name="name"
+                className="input"
+                placeholder="name"
+                required
+              />
               {/* photo url */}
               <label className="label">Photo URL</label>
-              <input type="text" className="input" placeholder="photo url" />
+              <input
+                type="text"
+                name="photo"
+                className="input"
+                placeholder="photo url"
+                required
+              />
               {/* email */}
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+                required
+              />
               {/* password */}
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input
+                type="password"
+                name="password"
+                className="input"
+                placeholder="Password"
+                required
+              />
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
